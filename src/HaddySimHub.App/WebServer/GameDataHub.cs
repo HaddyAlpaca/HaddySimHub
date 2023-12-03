9d81f3443097140064a041a6ff95450406bbe8fb
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2023-12-03 10:48:21 +0100

    Remove fixed demo mode

--- src/HaddySimHub.App/WebServer/GameDataHub.cs
+++ src/HaddySimHub.App/WebServer/GameDataHub.cs
@@ -12,10 +12,5 @@ public class GameDataHub : Hub
 
         //Init notification service
         NotificationService.Init(hubContext);
-
-        //watcher.GameDataIdle += async (s, e) => await _hubContext.Clients.All.SendAsync("gameDataIdle");
-        //watcher.RawDataUpdated += async (s, data) => await _hubContext.Clients.All.SendAsync("rawData", data);
-        //watcher.TruckDataUpdated += async (s, data) => await _hubContext.Clients.All.SendAsync("truckData", data);
-        //watcher.RaceDataUpdated += async (s, data) => await _hubContext.Clients.All.SendAsync("raceData", data);
     }
 }
