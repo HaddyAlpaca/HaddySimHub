21760fda5e543bd8860173ef451a9a045d7fd2d3
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2024-05-20 20:31:39 +0200

    Dirt 2 fix completed percentage

--- src/HaddySimHub.DirtRally2/DashboardDisplay.cs
+++ src/HaddySimHub.DirtRally2/DashboardDisplay.cs
@@ -15,7 +15,7 @@ public sealed class DashboardDisplay : IDisplay
             Rpm = Convert.ToInt32(typedData.rpm * 10),
             MaxRpm = Convert.ToInt32(typedData.max_rpm),
             Gear = Convert.ToInt32(typedData.gear),
-            CompletedPct = Math.Max(Convert.ToInt32(typedData.progress * 100), 100),
+            CompletedPct = Math.Min(Convert.ToInt32(typedData.progress * 100), 100),
             DistanceTravelled = Math.Max(Convert.ToInt32(typedData.distance), 0),
             Position = Convert.ToInt32(typedData.car_pos),
             Sector1Time = typedData.sector_1_time,
