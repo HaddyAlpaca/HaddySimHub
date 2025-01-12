41408af4558dbc1d904ef16ba05a6a50f49e0981
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2025-01-12 14:25:10 +0100

    Add attach trailer and confirm actions

---
+++ HaddyTruckSDPlugin/AttachTrailerAction.cs
@@ -0,0 +1,15 @@
+﻿using BarRaider.SdTools;
+
+namespace HaddyTruckSDPlugin
+{
+    [PluginActionId("com.haddyalpaca.truck.attach-trailer")]
+
+    internal class AttachTrailerAction(ISDConnection connection, InitialPayload payload) :
+        KeyActionBase(connection, payload)
+    {
+        protected override string GetActionKeys() => "T";
+
+        protected override string GetStateImage() =>
+            this._truckData.NumberOfTrailersAttached > 0 ? "on" : "off"; 
+    }
+}
---
+++ HaddyTruckSDPlugin/ConfirmExecuteAction.cs
@@ -0,0 +1,13 @@
+﻿using BarRaider.SdTools;
+
+namespace HaddyTruckSDPlugin
+{
+    [PluginActionId("com.haddyalpaca.truck.confirm-execute")]
+    public class ConfirmExecuteAction(ISDConnection connection, InitialPayload payload)
+        : KeyActionBase(connection, payload)
+    {
+        protected override string GetActionKeys() => "{ENTER}";
+
+        protected override string GetStateImage() => "icon";
+    }
+}
