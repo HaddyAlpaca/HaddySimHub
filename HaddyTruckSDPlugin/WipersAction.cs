52428d3b96f8db5c9647160d5aca267f57fb338d
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2025-01-28 19:32:20 +0100

    Add wipers

---
+++ HaddyTruckSDPlugin/WipersAction.cs
@@ -0,0 +1,12 @@
+﻿using BarRaider.SdTools;
+
+namespace HaddyTruckSDPlugin;
+
+[PluginActionId("com.haddyalpaca.truck.wipers")]
+public class WipersAction(ISDConnection connection, InitialPayload payload) : 
+    KeyActionBase(connection, payload)
+{
+    protected override string GetActionKeys() => "P";
+
+    protected override string GetStateImage() => this._truckData.WipersOn ? "on" : "off";
+}
