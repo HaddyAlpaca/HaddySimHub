dc9e4dc7fbbfa601a7e98dbcea0ce05e36bc677c
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2025-02-18 19:07:28 +0100

    Refactor SD plugin

---
+++ HaddyTruckSDPlugin/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using GregsStack.InputSimulatorStandard;
+
+namespace HaddyTruckSDPlugin;
+
+internal static class Helpers
+{
+    private static readonly InputSimulator _inputSimulator = new();
+
+    public static void SendKeys(string keys)
+    {
+        if (string.IsNullOrEmpty(keys))
+            return;
+
+        _inputSimulator.Keyboard.TextEntry(keys);
+    }
+}
--- HaddyTruckSDPlugin/KeyActionBase.cs
+++ HaddyTruckSDPlugin/KeyActionBase.cs
@@ -1,128 +1,83 @@
 ﻿using BarRaider.SdTools;
-using GregsStack.InputSimulatorStandard;
 using HaddySimHub.Shared;
-using Microsoft.AspNetCore.SignalR.Client;
-using System.Text.Json;
 
-namespace HaddyTruckSDPlugin
+namespace HaddyTruckSDPlugin;
+
+public abstract class KeyActionBase : KeypadBase
 {
-    public abstract class KeyActionBase : KeypadBase
+    private readonly SignalRConnection _signalR = new();
+    private string? _currentImage = null;
+    private string? _currentTitle = null;
+    private readonly string _plugInId;
+
+    protected TruckData _truckData = new();
+
+    public KeyActionBase(ISDConnection connection, InitialPayload payload)
+        : base(connection, payload)
     {
-        private readonly HubConnection _hubConnection;
-        private readonly InputSimulator _inputSimulator = new();
-        private bool _signalRStarted = false;
-        private string? _currentImage = null;
-        private string? _currentTitle = null;
+        this._plugInId = this.GetPluginId();
 
-        protected TruckData _truckData = new();
-
-        public KeyActionBase(ISDConnection connection, InitialPayload payload)
-            : base(connection, payload)
+        this._signalR.DataUpdated += async (s, e) =>
         {
-            this._hubConnection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:3333/display-data")
-                .WithAutomaticReconnect()
-                .Build();
+            this._truckData = e;
+            await this.UpdateState();
+        };
+    }
 
-            this._hubConnection.On<DisplayUpdate>("displayUpdate", async update =>
-            {
-                if (update.Type == DisplayType.TruckDashboard &&
-                    update.Data is not null &&
-                    update.Data is JsonElement element)
-                {
-                    var options = new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    };
+    public override void KeyPressed(KeyPayload payload) { }
 
-                    var data = element.Deserialize<TruckData>(options);
-                    if (data is not null)
-                    {
-                        this._truckData = data;
-                        await this.UpdateState();
-                    }
-                }
-            });
+    public override void KeyReleased(KeyPayload payload) => Helpers.SendKeys(this.GetActionKeys());
+
+    public override void OnTick()
+    {
+        try
+        {
+            this._signalR.StartIfNeeded();
         }
-
-        public override void KeyPressed(KeyPayload payload) { }
-
-        public override void KeyReleased(KeyPayload payload)
+        catch (Exception ex)
         {
-            string keys = this.GetActionKeys();
-            if (!string.IsNullOrEmpty(keys))
-            {
-                this._inputSimulator.Keyboard.TextEntry(keys);
-            }
-        }
-
-        public override void OnTick()
-        {
-            if (!this._signalRStarted)
-            {
-                try
-                {
-                    this._hubConnection.StartAsync().Wait();
-                    this._signalRStarted = true;
-                }
-                catch (Exception ex)
-                {
-                    Logger.Instance.LogMessage(TracingLevel.ERROR, ex.Message);
-                }
-            }
-        }
-
-        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
-
-        public override void ReceivedSettings(ReceivedSettingsPayload payload) { }
-
-        public override void Dispose()
-        {
-            try
-            {
-                this._hubConnection.StopAsync().Wait();
-            }
-            catch (Exception ex)
-            {
-                Logger.Instance.LogMessage(TracingLevel.ERROR, ex.Message);
-            }
-
-            GC.SuppressFinalize(this);
-        }
-
-        private async Task UpdateState()
-        {
-            string image = this.GetStateImage();
-            if (this._currentImage is null || this._currentImage != image)
-            {
-                this._currentImage = image;
-                await Connection.SetImageAsync(Path.Combine("images", GetPluginId(), image));
-            }
-
-            string title = this.GetTitle();
-            if (this._currentTitle is null || this._currentTitle != title)
-            {
-                await Connection.SetTitleAsync(this.GetTitle());
-            }
-        }
-
-        protected abstract string GetActionKeys();
-
-        protected abstract string GetStateImage();
-
-        protected virtual string GetTitle() => string.Empty;
-
-        private async Task SetImage(string imageFile) =>
-            await Connection.SetImageAsync(Path.Combine("images", GetPluginId(), imageFile));
-
-        private string GetPluginId()
-        {
-            // Get the PluginActionId attribute value
-            var attribute = (Attribute
-                .GetCustomAttributes(this.GetType(), typeof(PluginActionIdAttribute))
-                .FirstOrDefault()) as PluginActionIdAttribute;
-
-            return attribute?.ActionId.Split('.').Last() ?? string.Empty;
+            Logger.Instance.LogMessage(TracingLevel.ERROR, ex.Message);
         }
     }
+
+    public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+    public override void ReceivedSettings(ReceivedSettingsPayload payload) { }
+
+    public override void Dispose()
+    {
+        try
+        {
+            this._signalR.Stop();
+        }
+        catch (Exception ex)
+        {
+            Logger.Instance.LogMessage(TracingLevel.ERROR, ex.Message);
+        }
+
+        GC.SuppressFinalize(this);
+    }
+
+    private async Task UpdateState()
+    {
+        string image = this.GetStateImage();
+        if (this._currentImage != image)
+        {
+            await Connection.SetImageAsync(Path.Combine("images", this._plugInId, image));
+            this._currentImage = image;
+        }
+
+        string title = this.GetTitle();
+        if (this._currentTitle != title)
+        {
+            await Connection.SetTitleAsync(title);
+            this._currentTitle = title;
+        }
+    }
+
+    protected abstract string GetActionKeys();
+
+    protected abstract string GetStateImage();
+
+    protected virtual string GetTitle() => string.Empty;
 }
