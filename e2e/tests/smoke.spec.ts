158213eab84858663cd995bfa70cd258f821692d
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2025-03-21 18:08:10 +0100

    Remove e2e

--- e2e/tests/smoke.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Smoke when server cannot be reached', async ({ page }) => {
-  await page.goto('http://localhost:3333/');
-});
-
-test('Rally display is shown', async ({ page }) => {
-
-});
