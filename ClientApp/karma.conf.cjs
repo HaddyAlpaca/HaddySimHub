d1aed3c0aef45d7ad6e45f87ee8c50f5e6f06070
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2025-03-22 09:51:47 +0100

    Buildermode application for tests

--- ClientApp/karma.conf.cjs
+++ ClientApp/karma.conf.cjs
@@ -3,23 +3,16 @@ module.exports = function (config) {
     config.set({
       // Base path that will be used to resolve all patterns (eg. files, exclude)
       basePath: '',
-      
-      // Frameworks to use
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      
-      // List of plugins to load
+      frameworks: ['jasmine'],
       plugins: [
         require('karma-jasmine'),
         require('karma-chrome-launcher'),
-        require('@angular-devkit/build-angular/plugins/karma'),
         require('karma-spec-reporter')
       ],
-      
       client: {
         clearContext: false
       },
       reporters: ['spec'],
-
       specReporter: {
         maxLogLines: 5,             // limit number of lines logged per test
         suppressSummary: false,      // do not print summary
