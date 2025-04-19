d34182f9dd19ea282e4b2364351640cace7cb6f4
HaddyAlpaca <126945533+HaddyAlpaca@users.noreply.github.com>
2025-04-19 22:06:19 +0200

    Update TrackPosition.cs

--- HaddySimHub/Models/TrackPosition.cs
+++ HaddySimHub/Models/TrackPosition.cs
@@ -1,12 +1,12 @@
 namespace HaddySimHub.Models;
 
-public record TrackPosition
+public record TimingEntry
 {
     public string Name { get; init; } = string.Empty;
     public string CarNumber { get; init; } = string.Empty;
     public string License { get; init; } = string.Empty;
     public string LicenseColor { get; init; } = string.Empty;
     public long IRating { get; init; }
-    public float LapDistPct { get; init; }
-    public TrackPositionStatus Status { get; init; }
+    public int Lap { get; init; }
+    public float DistancePct { get; init; }
 }
