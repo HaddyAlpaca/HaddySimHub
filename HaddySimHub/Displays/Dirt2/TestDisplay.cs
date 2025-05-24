f400dd64f2cefe7d6519a3204dc7cdfe1d9b2b8b
HaddyAlpaca <126945533+HaddyAlpaca@users.noreply.github.com>
2025-05-24 22:05:38 +0200

    Add RPM max to rally test display

--- HaddySimHub/Displays/Dirt2/TestDisplay.cs
+++ HaddySimHub/Displays/Dirt2/TestDisplay.cs
@@ -16,6 +16,7 @@ namespace HaddySimHub.Displays.Dirt2
                     DistanceTravelled = (short)DateTime.Now.Millisecond,
                     Gear = new Random().Next(1, 6).ToString(),
                     Rpm = new Random().Next(0, 10000),
+                    RpmMax = 7000,
                     LapTime = new Random().Next(0, 100),
                     Sector1Time = new Random().Next(0, 100),
                     Sector2Time = new Random().Next(0, 100),
