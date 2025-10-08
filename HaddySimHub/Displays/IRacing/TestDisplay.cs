eae860d8ccfaaa06bc474d2ff36102f599513c26
HaddyAlpaca <126945533+HaddyAlpaca@users.noreply.github.com>
2025-10-08 18:21:59 +0200

    Add required tire temperature fields to RaceData
    Marked tire temperature properties in RaceData as required and updated TestDisplay to generate random tire temperature values. This ensures tire temperature data is always provided when creating RaceData instances.

--- HaddySimHub/Displays/IRacing/TestDisplay.cs
+++ HaddySimHub/Displays/IRacing/TestDisplay.cs
@@ -44,6 +44,10 @@ namespace HaddySimHub.Displays.IRacing
                     BrakePct = brakePct,
                     ThrottlePct = throttlePct,
                     CarNumber = "80",
+                    TireTempLF = new Random().Next(40, 100),
+                    TireTempRF = new Random().Next(40, 100),
+                    TireTempLR = new Random().Next(40, 100),
+                    TireTempRR = new Random().Next(40, 100),
                 }
             };
         }
