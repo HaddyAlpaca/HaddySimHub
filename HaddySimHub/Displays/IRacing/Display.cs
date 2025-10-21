dd34dd32de087650053b65ce817d4078c619f6fd
HaddyAlpaca <126945533+HaddyAlpaca@users.noreply.github.com>
2025-10-21 21:20:13 +0200

    Update Display.cs

--- HaddySimHub/Displays/IRacing/Display.cs
+++ HaddySimHub/Displays/IRacing/Display.cs
@@ -167,7 +167,7 @@ internal sealed class Display() : DisplayBase<DataSample>()
             PitLimiterOn = telemetry.EngineWarnings.HasFlag(EngineWarnings.PitSpeedLimiter),
             CarNumber = playerInfo?.CarNumber ?? string.Empty,
             // Convert steering angle to percentage (0-100)
-            SteeringPct = (int)Math.Round((telemetry.SteeringWheelAngle / telemetry.SteeringWheelAngleMax + 1) * 50)
+            SteeringPct = (int)Math.Round(((telemetry.SteeringWheelAngle / telemetry.SteeringWheelAngleMax) * 100) + 50)
         };
         return new DisplayUpdate { Type = DisplayType.RaceDashboard, Data = displayUpdate };
     }
